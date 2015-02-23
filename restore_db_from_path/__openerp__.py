b056f81ff2d873fa7e6e48a42b7e37acfacd57b3
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-02-23 09:17:43 -0300

    ADD restore_db_from_path

---
+++ restore_db_from_path/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Restore DB From Path",
+    "version": "1.0",
+    "author": "Ingenieria ADHOC",
+    "complexity": "normal",
+    "description": """
+    """,
+    "category": "Tools",
+    "depends": [
+    ],
+    "data": [
+    ],
+    "js": [
+    ],
+    "css": [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "external_dependencies": {
+        'python': [],
+    },
+}
